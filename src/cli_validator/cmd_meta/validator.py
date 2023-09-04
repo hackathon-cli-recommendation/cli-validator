fabd8b0d049186dba4834a977dd13ed8a4e2d485
Aldo Olascoaga Olmedo <69182604+Aldo-Olascoaga@users.noreply.github.com>
2023-09-03 21:09:12 -0700

    validate_command function updated

--- src/cli_validator/cmd_meta/validator.py
+++ src/cli_validator/cmd_meta/validator.py
@@ -1,10 +1,12 @@
 from cli_validator.cmd_meta.loader import load_metas, build_command_tree
 from cli_validator.cmd_meta.parser import CLIParser
 from cli_validator.cmd_tree import parse_command
+from cli_validator.exceptions import ValidateFailureException
 
 
 class CommandMetaValidator(object):
     """A validator using Command Metadata generated from breaking change tool"""
+
     def __init__(self, version: str, cache_dir: str = './cmd_meta'):
         """
         :param version: the version of `azure-cli` that provides the metadata
@@ -26,6 +28,22 @@ class CommandMetaValidator(object):
         parser = self.build_parser(meta)
         namespace = parser.parse_args(cmd.parameters)
 
+        if namespace.ids is not None:
+            for param in meta['parameters']:
+                if 'id_part' in param:
+                    continue
+                else:
+                    if 'required' in param and namespace.__getattribute__(param['name']) is None:
+                        raise ValidateFailureException('Some arguments are required)
+                    else:
+                        continue
+        else:
+            for param in meta['parameters']:
+                if 'required' in param and namespace.__getattribute__(param['name']) is None:
+                    raise ValidateFailureException('Some arguments are required)
+                else:
+                    continue
+
     def load_command_meta(self, signature, module):
         """
         Load metadata of specific command.
@@ -35,8 +53,8 @@ class CommandMetaValidator(object):
         """
         module_meta = self.metas[f'az_{module}_meta.json']
         meta = module_meta
-        for idx in range(len(signature)-1):
-            meta = meta['sub_groups'][' '.join(signature[:idx+1])]
+        for idx in range(len(signature) - 1):
+            meta = meta['sub_groups'][' '.join(signature[:idx + 1])]
         return meta['commands'][' '.join(signature)]
 
     def build_parser(self, meta):
