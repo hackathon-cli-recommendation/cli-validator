5349d5d777a278e9ac140b5fadfc1ab862fac2d9
Aldo Olascoaga Olmedo <69182604+Aldo-Olascoaga@users.noreply.github.com>
2023-09-04 00:20:18 -0700

    assertRaises added

--- tests/test_cmd_meta_validator.py
+++ tests/test_cmd_meta_validator.py
@@ -15,7 +15,7 @@ class TestCmdChangeValidator(TestCase):
         
     def test_validate_correct_command_with_ids(self):
         self.validator.validate_command('az storage account show --ids /subscriptions/0b1f6471-1bf0-4dda-aec3-cb9272f09590/resourceGroups/qinkai-test/providers/Microsoft.Storage/storageAccounts/storageqinkai')
-    
+
     def test_validate_correct_command_with_ids_and_separated_parameter_with_id_part(self):
         self.validator.validate_command('az storage account show --ids /subscriptions/0b1f6471-1bf0-4dda-aec3-cb9272f09590/resourceGroups/qinkai-test/providers/Microsoft.Storage/storageAccounts/storageqinkai -n storageqin')
     
@@ -23,7 +23,8 @@ class TestCmdChangeValidator(TestCase):
         self.validator.validate_command('az webapp create --ids /subscriptions/xxxxx/resourceGroups/xxxxx/xxxxx -p plan-test')
     
     def test_validate_incorrect_command_with_required_parameters_needed(self):
-        self.validator.validate_command('az webapp create --ids /subscriptions/xxxxx/resourceGroups/xxxxx/xxxxx')
+        with self.assertRaises(ValidateFailureException):
+            self.validator.validate_command('az webapp create --ids /subscriptions/xxxxx/resourceGroups/xxxxx/xxxxx')
     
     def test_validate_command_without_required_parameters(self):
         self.validator.validate_command('az sig list-community --location myLocation')
